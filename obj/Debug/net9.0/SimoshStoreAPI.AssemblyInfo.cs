6fc62ace653bc396377353b38756935e2a59a2a4
Mustafa Can Sargın <165585760+mustafas4rgin@users.noreply.github.com>
2025-03-06 18:20:10 +0300

    finished

--- obj/Debug/net9.0/SimoshStoreAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/SimoshStoreAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimoshStoreAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da53849c096d58e5f0bc2868c18e5d26ca60907f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c269f87e393be39859862d5dc8317d112d8007b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimoshStoreAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimoshStoreAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
